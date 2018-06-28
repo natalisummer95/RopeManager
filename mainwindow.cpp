31d5c488488b5426a3dd80df3d99fc83eb50164b
ChichkinM <ChichkinM@yandex.ru>
2018-06-28 23:18:45 +0300

    Hello.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,38 +8,32 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    settings = new QSettings("mysettings.ini",
-    QSettings:: IniFormat);
-};
+    ui ->setupUi(this);
 
-{
-   ui ->setupUi(this)
-   ui->tableWidget->setRowCount(10);
-   ui->tableWidget->setColumnCount(7);
+    settings = new QSettings("mysettings.ini", QSettings:: IniFormat);
 
-   ui->tableWidget->setHorizontalHeaderLabels(QStringList()<< "DMC"<< "Gamma"<<"Anchor"<<"Кирова"<<"Belka"<<"Madeira"<<"Цвет");
+    ui->tableWidget->setRowCount(10);
+    ui->tableWidget->setColumnCount(7);
 
-    int count = 1;
-    for (int i=0; i < ui->tableWidget->rowCount(); i++);
-        for(int j=0; j < ui->tableWidget->colorCount();j++);
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList()<< "DMC"<< "Gamma"<<"Anchor"<<"Кирова"<<"Belka"<<"Madeira"<<"Цвет");
 
-          QTableWidgetItem *itm = new QTableWidgetItem (tr("%1").arg(count));
+     int count = 1;
+     for (int i=0; i < ui->tableWidget->rowCount(); i++)
+         for(int j=0; j < ui->tableWidget->colorCount();j++) {
 
-          ui->tableWidget->setItem(i,j, itm);
-          count++;
+           QTableWidgetItem *itm = new QTableWidgetItem (QString("привет %1").arg(count));
 
- };
+           ui->tableWidget->setItem(i,j, itm);
+           count++;
+         }
+}
 
 MainWindow::~MainWindow()
 {
     saveSettings();
     delete ui;
-};
+}
 
-void MainWindow::saveSettings()
-{
-};
+void MainWindow::saveSettings() { }
 
-void MainWindow::loadSettings()
-{
-};
+void MainWindow::loadSettings() { }
